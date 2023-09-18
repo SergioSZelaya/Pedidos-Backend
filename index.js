aaae2636787ac2a849913d74d88aa8ecfaf4a1f6
Sergio Zelaya <sergiozelaya90@gmail.com>
2023-09-18 20:31:32 -0300

    agrego variable para guardar puerto, 4000

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ import express from "express";
 
 console.log("funciona, mi back va bien");
 //1- configuraciones iniciales
-
+const app = express();
+//crear variable con express
+app.set("port", process.env.PORT || 4000);
 //2- configurar middlewares (permite recibir solicitudes ext)
 
 //3- crear las rutas (siempre despues de los middlewares)
