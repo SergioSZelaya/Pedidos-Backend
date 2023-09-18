bbab5e2869033518fd52503ee45c758c477ca6b1
Sergio Zelaya <sergiozelaya90@gmail.com>
2023-09-18 20:12:14 -0300

    se configuran los modulos

---
+++ index.js
@@ -0,0 +1,8 @@
+import express from "express";
+
+console.log("funciona, mi back va bien");
+//1- configuraciones iniciales
+
+//2- configurar middlewares (permite recibir solicitudes ext)
+
+//3- crear las rutas (siempre despues de los middlewares)
