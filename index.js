ddc13206ec47b716f508aeef77faa343d534722b
Sergio Zelaya <sergiozelaya90@gmail.com>
2023-09-18 21:26:50 -0300

    configuracion de middlewares

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import "dotenv/config";
 
 console.log("funciona, mi back va bien");
 //1- configuraciones iniciales
 const app = express();
 //crear variable con express
 app.set("port", process.env.PORT || 4000);
-//2- configurar middlewares (permite recibir solicitudes ext)
+app.listen(app.get("port"), () => {
+  console.log("Estoy en el puerto ..." + app.get("port"));
+});
+//2- configurar middlewares (funciones que permiten recibir solicitudes ext)
+
+app.use(cors()); //permite conexiones remotas
+app.use(express.json()); //permite que entienda la solicitud enviada al backend en formato json
+app.use(express.urlencoded({ extended: true })); // le pido ayuda a express para que entienda un array o string, datos que vienen del body del request
+app.use(morgan("dev")); //nos da mas informacion en la terminal
 
 //3- crear las rutas (siempre despues de los middlewares)
