3a25c50011df81ad4716e43752ab8d5f7db33f48
unknown <isaiaspadros2@gmail.com>
2023-09-20 19:11:05 -0300

    Borrado y correcion de mensajes de retorno de funciones

--- src/controllers/usuario.controllers.js
+++ src/controllers/usuario.controllers.js
@@ -1,34 +1,28 @@
 import Usuario from "../models/usuario.js";
-import bcrypt from 'bcrypt';
+import bcrypt from "bcrypt";
 
 export const login = async (req, res) => {
-    try {
-      //verificar si existe un mail como el recibido
-      const { email, password } = req.body;
-  
-      //verificar si el email ya existe
-      //email = req.body.email
-      let usuario = await Usuario.findOne({ email });
-      if (!usuario) {
-        //si el usuario existe
-        return res.status(400).json({
-          mensaje: "Correo o password invalido - correo",
-        });
-      }
-      const passwordValido = bcrypt.compareSync(password, usuario.password);
-    if(!passwordValido){
-       return res.status(400).json({mensaje:'Correo o password invalido- password'})
+  try {
+    const { email, password } = req.body;
+    let usuario = await Usuario.findOne({ email });
+    if (!usuario) {
+      return res.status(400).json({
+        mensaje: "Correo o password invalido",
+      });
     }
-
-    //responder que el usuario es correcto
-    res.status(200).json({
+    const passwordValido = bcrypt.compareSync(password, usuario.password);
+    if (!passwordValido) {
+      return res.status(400).json({ mensaje: "correo o Password invalido" });
+    }
+     res.status(200).json({
       mensaje: "El usuario existe",
       uid: usuario._id,
       nombre: usuario.nombreUsuario,
     });
-    } catch (error) {
-        console.log(error);
-        res.status(400).json({
-          mensaje: "usuario o contraseña invalido",
-        });
-      }}
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({
+      mensaje: "usuario o contraseña no valido",
+    });
+  }
+};
