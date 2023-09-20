2bd82b193408029bf69e36c67755b80726d38377
unknown <isaiaspadros2@gmail.com>
2023-09-20 18:56:27 -0300

    Agregado de estructura principal try catch de login

--- src/controllers/usuario.controllers.js
+++ src/controllers/usuario.controllers.js
@@ -1,2 +1,23 @@
 import Usuario from "../models/usuario.js";
 import bcrypt from 'bcrypt';
+
+export const login = async (req, res) => {
+    try {
+      //verificar si existe un mail como el recibido
+      const { email, password } = req.body;
+  
+      //verificar si el email ya existe
+      //email = req.body.email
+      let usuario = await Usuario.findOne({ email });
+      if (!usuario) {
+        //si el usuario existe
+        return res.status(400).json({
+          mensaje: "Correo o password invalido - correo",
+        });
+      }
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({
+          mensaje: "usuario o contraseña invalido",
+        });
+      }}
