aa6a7d1f8dc86e88f0e41489d983baae38242a66
unknown <isaiaspadros2@gmail.com>
2023-09-20 21:01:03 -0300

    Cambio de nombres en esp a eng

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,23 +1,23 @@
-import Usuario from "../models/usuario.js";
+import User from "../models/user.js";
 import bcrypt from "bcrypt";
 
 export const login = async (req, res) => {
   try {
     const { email, password } = req.body;
-    let usuario = await Usuario.findOne({ email });
-    if (!usuario) {
+    let user = await User.findOne({ email });
+    if (!user) {
       return res.status(400).json({
         mensaje: "Correo o password invalido",
       });
     }
-    const passwordValido = bcrypt.compareSync(password, usuario.password);
+    const passwordValido = bcrypt.compareSync(password, user.password);
     if (!passwordValido) {
       return res.status(400).json({ mensaje: "correo o Password invalido" });
     }
      res.status(200).json({
       mensaje: "El usuario existe",
-      uid: usuario._id,
-      nombre: usuario.nombreUsuario,
+      uid: user._id,
+      nombre: user.nickName,
     });
   } catch (error) {
     console.log(error);
--- src/routes/usuario.routes.js
+++ src/routes/usuario.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { login } from "../controllers/usuario.controllers";
+import { login } from "../controllers/user.controllers.js";
 
 const router = Router();
 
