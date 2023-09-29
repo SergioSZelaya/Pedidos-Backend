d57b938b945fea28680d175e2058b52345bae3fa
dharez <mauroromo.developer@outlook.com>
2023-09-28 22:03:46 -0300

    agregando el modelo de usuarios en la carpeta de models

---
+++ src/models/usuario.js
@@ -0,0 +1,36 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new Schema({
+  nickname: {
+    type: String,
+    required: true,
+    unique: true,
+    minLength: 2,
+    maxLength: 20,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    minLength: 6,
+    maxLength: 150,
+  },
+  password: {
+    type: String,
+    required: true,
+    minLength: 8,
+    maxLength: 200,
+  },
+  rol: {
+    type: String,
+    required: true,
+  },
+  state: {
+    type: Boolean,
+    default: true,
+  },
+});
+
+const User = mongoose.model("usuario", userSchema);
+
+export default User;
