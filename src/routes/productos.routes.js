0004cfec71e84f82abb6905ccf9568296e42c8c3
Rivadeneiea <joserivadeneira538@gmail.com>
2023-10-01 12:51:51 -0300

    agrego validaciones para image y price

--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -19,7 +19,31 @@ router
     [
       check("nameProduct")
         .notEmpty()
-        .withMessage("El nombre del producto es un dato obligatorio"),
+        .withMessage("El nombre del producto es un dato obligatorio")
+        .isLength({
+          min: 2,
+          max: 50,
+        })
+        .withMessage(
+          "EL nombre del producto debe tener entre 2 y 5 caracteres"
+        ),
+      check("price")
+        .notEmpty()
+        .withMessage("el precio es un dato obligatorio")
+        .isNumeric()
+        .withMessage("El precio debe ser un numero")
+        .custom((value) => {
+          if (value >= 1 && value <= 10000) {
+            return true;
+          } else {
+            throw new Error("el precio debe estar entre 1 y 10000");
+          }
+        }),
+      check("image")
+        .notEmpty()
+        .withMessage("la imagen es un dato obligatorio")
+        .matches(/^https?:\/\/[\w\-]+(\.[\w\-]+)+[/#?]?.*$/)
+        .withMessage("Debe ingresar una url de una imagen valida"),
     ],
     createProducts
   );
