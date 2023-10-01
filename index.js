a1de2fd66a144e5840911fe7f9f3632260ceef36
Sergio Zelaya <sergiozelaya90@gmail.com>
2023-10-01 17:08:18 -0300

    actualizando rama dev con los cambios de rama orders de forma local ya que no se pudo realizar el pull-request, además se limpia el código de archivos vacíos, también se agrega al index.js un archivo estático para poder mostrar mensaje de pagina backend

--- index.js
+++ index.js
@@ -3,13 +3,18 @@ import cors from "cors";
 import morgan from "morgan";
 import "dotenv/config";
 import productoRouter from "./src/routes/productos.routes.js"; //esto se cambia (productoRouter)porque hay que crear ruta y controlador para usuarios
-
+import orderRouter from "./src/routes/orders.routes.js";
 import "./src/database/databaseMauro.js";
 import userRouter from "./src/routes/users.routes.js";
+import path from "path";
+import { fileURLToPath } from "url";
 
 console.log("funciona, mi back va bien");
 //1- configuraciones iniciales
 const app = express();
+const _filename = fileURLToPath(import.meta.url);
+const _dirname = path.dirname(_filename);
+
 //crear variable con express
 app.set("port", process.env.PORT || 4000);
 app.listen(app.get("port"), () => {
@@ -21,9 +26,12 @@ app.use(cors()); //permite conexiones remotas
 app.use(express.json()); //permite que entienda la solicitud enviada al backend en formato json
 app.use(express.urlencoded({ extended: true })); // le pido ayuda a express para que entienda un array o string, datos que vienen del body del request
 app.use(morgan("dev")); //nos da mas informacion en la terminal
-
+//agregar archivo estatico
+      app.use(
+  express.static(
+    path.join(_dirname, '/public')));
 //3- crear las rutas (siempre despues de los middlewares)
 // http://localhost:4000/api/producto
 app.use("/api", productoRouter); //esto se cambia porque hay que crear ruta y controlador para usuarios
-
+app.use("/api", orderRouter);
 app.use("/api/usuario", userRouter);
